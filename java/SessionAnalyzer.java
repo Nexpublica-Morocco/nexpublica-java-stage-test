d3c3d4a376078c0d6836c1edc0102b074cc1e471
elazridriss-ux <elazridriss@icloud.com>
2026-03-10 23:46:58 +0000

    Create SessionAnalyzer class with calculateSessionTime method
    Implemented a basic structure for SessionAnalyzer class with a method to calculate session time.

---
+++ java/SessionAnalyzer.java
@@ -0,0 +1,9 @@
+public class SessionAnalyzer {
+
+    public Map<String, Long> calculateSessionTime(List<Event> events) {
+
+        // TODO implement logic
+
+        return new HashMap<>();
+    }
+}
